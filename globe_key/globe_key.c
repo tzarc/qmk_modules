8d3e35333e7987d478360d067a22c7a0889b6ff0
Nick Brassel <nick@tzarc.org>
2025-02-26 23:26:41 +1100

    More mucking about, cleanup, readmes and a globe key.

---
+++ globe_key/globe_key.c
@@ -0,0 +1,16 @@
+// Copyright 2025 Nick Brassel (@tzarc)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#include QMK_KEYBOARD_H
+
+bool process_record_globe_key(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_GLOBE:
+            if (record->event.pressed) {
+                host_consumer_send(AC_NEXT_KEYBOARD_LAYOUT_SELECT);
+            } else {
+                host_consumer_send(0);
+            }
+            break;
+    }
+    return false;
+}
